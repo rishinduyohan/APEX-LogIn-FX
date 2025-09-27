2fca0222102a5ad6e03701b29b9236ba46c369d7
rishinduyohan <rishinduyohan@gmail.com>
2025-09-27 17:23:20 +0530

    Print username and password

--- src/main/java/controller/LogIn_Controller.java
+++ src/main/java/controller/LogIn_Controller.java
@@ -1,4 +1,18 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LogIn_Controller {
+    public TextField txtUsername;
+    public PasswordField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+        System.out.println("Username : "+txtUsername.getText()+"\nPassword : "+txtPassword.getText());
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
